c22eb851977aa5338dbe45c6262c6c829631ba4b
Nikita Kholin <nik.kholin@gmail.com>
2017-01-29 15:44:30 +0200

    Add no header option to application layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def index
     if !user_signed_in?
-      render :user_not_signed_in, layout: false
+      @no_header = true
+      render :user_not_signed_in
     elsif !current_user.room?
       render :user_has_no_room
     end
