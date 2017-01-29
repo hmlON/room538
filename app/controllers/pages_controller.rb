d36bbc507c1101b765f31c1374fef050879dae76
Nikita Kholin <nik.kholin@gmail.com>
2017-01-29 14:21:36 +0200

    Add welcome page when user is not signed in and when user has no room

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,9 @@
 class PagesController < ApplicationController
-  def index; end
+  def index
+    if !user_signed_in?
+      render :user_not_signed_in, layout: false
+    elsif !current_user.room?
+      render :user_has_no_room
+    end
+  end
 end
