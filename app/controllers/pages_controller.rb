de3dc1cf1cd1704b827c603409f6d11b72380a07
Nikita Kholin <nik.kholin@gmail.com>
2017-01-31 17:45:42 +0200

    Create page when user has no room

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
       @no_header = true
       render :user_not_signed_in
     elsif !current_user.room?
+      @no_header = true
       render :user_has_no_room
     else
       redirect_to rooms_path
