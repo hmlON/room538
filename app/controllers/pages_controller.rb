80265e90b416c71c098be65b121b88ed4bfb7d4c
Nikita Kholin <nik.kholin@gmail.com>
2017-01-31 22:58:47 +0200

    Replace navbar with settings icon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
   def index
     if !user_signed_in?
-      @no_header = true
       render :user_not_signed_in
     elsif !current_user.room?
-      @no_header = true
       render :user_has_no_room
     else
       redirect_to rooms_path
