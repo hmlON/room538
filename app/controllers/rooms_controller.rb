2eae86be5065f9e6d28066be6dfde42620910367
Nikita Kholin <nik.kholin@gmail.com>
2017-02-04 12:08:17 +0200

    Add actions select to room form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -47,6 +47,6 @@ class RoomsController < ApplicationController
   end
 
   def room_params
-    params.require(:room).permit(:name)
+    params.require(:room).permit(:name, action_ids: [])
   end
 end
