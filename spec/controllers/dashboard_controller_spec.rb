7d9dfd48c477351905cdbc8851a372de340784a6
Nikita Kholin <nik.kholin@gmail.com>
2017-02-04 22:33:42 +0200

    Fix dashboard spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,8 +1,19 @@
 RSpec.describe DashboardController, type: :controller do
-  describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+  include Devise::Test::ControllerHelpers
+
+  context 'when user signed in' do
+    let(:user) { create(:user) }
+    before(:each) do
+      sign_in user
+      room = create(:room)
+      room.users << user
+    end
+
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
