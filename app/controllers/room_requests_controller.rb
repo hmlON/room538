24908a81022b6bba91d1ee03cec47398fbb70a3c
Nikita Kholin <nik.kholin@gmail.com>
2017-02-06 15:31:15 +0200

    Add create room request action

--- app/controllers/room_requests_controller.rb
+++ app/controllers/room_requests_controller.rb
@@ -1,5 +1,19 @@
 class RoomRequestsController < ApplicationController
   def index; end
 
-  def create; end
+  def create
+    request = RoomRequest.new(request_params)
+    request.sender = current_user
+    if request.save
+      redirect_to rooms_path, notice: 'Request successfullt sent.'
+    else
+      redirect_to rooms_path, alert: 'There was an error sending your request'
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:room_request).permit(:room_id)
+  end
 end
