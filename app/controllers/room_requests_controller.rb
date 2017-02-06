8548bff4ffb39843a9fe2c7eaefd865acbf2c21b
Nikita Kholin <nik.kholin@gmail.com>
2017-02-06 19:42:23 +0200

    Add accept action to room requests

--- app/controllers/room_requests_controller.rb
+++ app/controllers/room_requests_controller.rb
@@ -1,22 +1,29 @@
 class RoomRequestsController < ApplicationController
+  before_action :set_request, only: [:accept, :destroy]
+
   def index
     @requests_to_room = current_user.room.room_requests if current_user.room?
     @sent_requests = current_user.sent_room_requests
+  end
+
+  def accept
+    sender = User.find(@request.sender_id)
+    sender.join_room(current_user.room)
+    redirect_to dashboard_path, notice: "#{sender.name} successfully joined your room."
   end
 
   def create
     request = RoomRequest.new(request_params)
     request.sender = current_user
     if request.save
-      redirect_to rooms_path, notice: 'Request successfullt sent.'
+      redirect_to rooms_path, notice: 'Request successfully sent.'
     else
       redirect_to rooms_path, alert: 'There was an error sending your request'
     end
   end
 
   def destroy
-    request = RoomRequest.find(params[:id])
-    if request.destroy
+    if @request.destroy
       redirect_to rooms_path, notice: 'Request successfullt deleted.'
     else
       redirect_to rooms_path, alert: 'There was an error deleting your request'
@@ -24,6 +31,10 @@ class RoomRequestsController < ApplicationController
   end
 
   private
+
+  def set_request
+    @request = RoomRequest.find(params[:id])
+  end
 
   def request_params
     params.require(:room_request).permit(:room_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard' => 'dashboard#index'
   root 'pages#index'
+  post 'room_requests/accept/:id' => 'room_requests#accept', as: 'accept_room_request'
   resources :room_requests, only: [:index, :create, :destroy]
   resources :rooms, only: [:index, :new, :create]
   get '/rooms/edit' => 'rooms#edit', as: 'edit_room'
