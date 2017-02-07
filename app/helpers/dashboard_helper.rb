455001d46817736c1adc84f57ac5c57f7716caad
Nikita Kholin <nik.kholin@gmail.com>
2017-02-07 16:55:06 +0200

    Move progress bar into helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,14 @@
 module DashboardHelper
+  def progress_tag(current:, min: 0, max: 100)
+    width_persentage = current.zero? ? 0 : max / current
+    content_tag(:div,
+                content_tag(:div, '',
+                            class: 'progress-bar',
+                            'aria-valuemax' => max,
+                            'aria-valuemin' => min,
+                            'aria-valuenow' => current,
+                            'role' => 'progressbar',
+                            'style' => "width: #{width_persentage}%"),
+                class: 'progress')
+  end
 end
