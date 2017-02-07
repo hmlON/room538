903f8f45b86f9d87b0a0204a46dccba4077e0135
Nikita Kholin <nik.kholin@gmail.com>
2017-02-07 18:33:45 +0200

    Fix helper width calculation

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def progress_tag(current:, min: 0, max: 100)
-    width_persentage = current.zero? ? 0 : max / current
+    width_persentage = current.zero? ? 0 : current * 100 / max
     content_tag(:div,
                 content_tag(:div, '',
                             class: 'progress-bar',
