7e54f07c0a7fb1fb545aa1e5343c0b18dc409ee9
Nikita Kholin <nik.kholin@gmail.com>
2017-02-07 14:18:34 +0200

    Add user_actions to room_action model and dashboard

--- app/models/room_action.rb
+++ app/models/room_action.rb
@@ -2,4 +2,11 @@
 class RoomAction < ApplicationRecord
   belongs_to :room
   belongs_to :action
+
+  delegate :name, to: :action
+
+  def user_actions
+    ids = room.users.includes(:user_actions).pluck('user_actions.id')
+    UserAction.where(id: ids).where(action_id: action_id)
+  end
 end
