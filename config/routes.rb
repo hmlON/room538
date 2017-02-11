95845ea13399421c84394ca55b829fc9270154fe
Nikita Kholin <nik.kholin@gmail.com>
2017-02-11 21:00:33 +0200

    Fix accidently deleted room_request destroy route instead of room destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   patch 'room/leave' => 'rooms#leave', as: 'leave_room'
   post 'room_requests/accept/:id' => 'room_requests#accept', as: 'accept_room_request'
-  resources :room_requests, only: [:index, :create]
+  resources :room_requests, only: [:index, :create, :destroy]
   resources :rooms, only: [:index, :new, :create]
   get '/rooms/edit' => 'rooms#edit', as: 'edit_room'
   patch '/rooms' => 'rooms#update'
-  delete '/rooms' => 'rooms#destroy'
   devise_for :users
 end
