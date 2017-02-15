6e32435ade9a6dd0d0f3ee7f6fec018682d2bf3b
Nikita Kholin <nik.kholin@gmail.com>
2017-02-15 14:36:18 +0200

    Move action creation to modal

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -2,16 +2,12 @@ class ActionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_room_presence
 
-  def new
-    @action = Action.new
-  end
-
   def create
     @action = Action.new(action_params)
     if @action.save
       redirect_to :back, notice: "Action \"#{@action.name}\" has been successfully created"
     else
-      render :new
+      redirect_to :back, alert: 'There was an error creating this action!'
     end
   end
 
