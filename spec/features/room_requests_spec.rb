b72837e0e8ce02d662383b57d1189c6ef83764f4
Nikita Kholin <nik.kholin@gmail.com>
2017-02-16 19:18:51 +0200

    Add user joining room spec

---
+++ spec/features/room_requests_spec.rb
@@ -0,0 +1,25 @@
+RSpec.feature 'Room requests' do
+  let(:user) { create(:user) }
+  let(:room) { create(:room) }
+
+  background do
+    sign_in user
+  end
+
+  scenario 'User joins room' do
+    room_user = room.users.first
+
+    visit rooms_path
+    within "#room-#{room.id}" do
+      click_on 'Send request to join'
+    end
+    expect(page).to have_content 'Request successfully sent'
+
+    sign_in room_user
+    click_link 'Requests to join room'
+    click_button 'Accept'
+
+    expect(page).to have_content "#{user.name} successfully joined your room"
+    expect(user.reload.room).to eq room
+  end
+end
