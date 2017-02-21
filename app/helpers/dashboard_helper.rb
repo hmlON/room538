8277928b84547b318193b00ff3dafce2e67e3e63
Nikita Kholin <nik.kholin@gmail.com>
2017-02-21 18:37:26 +0200

    Add activity time styling

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -11,4 +11,11 @@ module DashboardHelper
                             'style' => "width: #{width_persentage}%"),
                 class: 'progress')
   end
+
+  def activity_time_tag(time)
+    content_tag(:span,
+                time_ago_in_words(time) + ' ago.',
+                class: 'activity--time',
+                title: time.strftime('%d %b %Y %T'))
+  end
 end
