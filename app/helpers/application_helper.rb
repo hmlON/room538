ce32370cf58e835ca4740cafb5d14dee5277b356
Nikita Kholin <nik.kholin@gmail.com>
2017-03-01 18:14:24 +0200

    Add dymanic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
   def flash_key_alert_class(key)
     flash_to_alert = {
       'notice' => 'info',
