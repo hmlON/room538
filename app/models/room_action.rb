e89cffbe303ce04c6ab9b00731e7b2247041c066
Nikita Kholin <nik.kholin@gmail.com>
2017-03-02 09:21:39 +0200

    Add creation of user actions on creation of room actions

--- app/models/room_action.rb
+++ app/models/room_action.rb
@@ -2,15 +2,11 @@
 class RoomAction < ApplicationRecord
   belongs_to :room
   belongs_to :action
-  has_many :user_actions
+  has_many :user_actions, dependent: :destroy
+  after_create :create_user_actions
 
   default_scope { includes(:action).order(:action_id) }
   delegate :name, to: :action
-
-  # def user_actions
-  #   ids = room.users.includes(:user_actions).pluck('user_actions.id')
-  #   UserAction.where(id: ids).where(action_id: action_id)
-  # end
 
   def max_value
     user_actions.pluck(:value).max
@@ -22,5 +18,13 @@ class RoomAction < ApplicationRecord
 
   def next_on_user
     user_actions.find_by(value: min_value).user
+  end
+
+  private
+
+  def create_user_actions
+    room.users.each do |user|
+      user_actions.create(user_id: user.id)
+    end
   end
 end
