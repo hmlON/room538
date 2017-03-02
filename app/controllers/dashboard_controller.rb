a9690c665758957aa7678f58dc8431b26d035b49
Nikita Kholin <nik.kholin@gmail.com>
2017-03-02 15:10:04 +0200

    Update sumbit action form and remove unnecessary variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,13 +3,11 @@ class DashboardController < ApplicationController
   before_action :require_room_presence
 
   def index
-    @room = Room.includes(:users, :actions).where(id: current_user.room_id).first
-    @users = @room.users
-    @actions = @room.actions
+    @room = Room.includes(:users, room_actions: :user_actions).where(id: current_user.room_id).first
   end
 
   def do_action
-    action = current_user.actions.where(action_id: params[:user_action][:id]).first
+    action = current_user.user_actions.find_by(id: params[:user_action][:id])
     action.update(value: action.value + 1)
     action.create_activity :done, owner: current_user
     redirect_to dashboard_path, notice: "Good job, #{current_user.name}!"
