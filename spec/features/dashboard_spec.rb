a97cc15a003e5e9a41fb3e6ade5c7f97df3a6534
Nikita Kholin <nik.kholin@gmail.com>
2017-03-02 15:08:15 +0200

    Update dashboard spec to use new user_actions associating

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature 'Dashboard' do
     expect {
       select room_action.name, from: 'user_action_id'
       click_button 'Submit'
-    }.to change { user.user_actions.where(action_id: room_action.action_id).first.value }.by(1)
+    }.to change { user.user_actions.find_by(room_action_id: room_action.id).value }.by(1)
 
     expect(page).to have_content 'Good job'
     expect(room_action.next_on_user).not_to eq user
