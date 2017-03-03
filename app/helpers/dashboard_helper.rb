a66acd2740756e43f010abad16887a12067a4f1b
Nikita Kholin <nik.kholin@gmail.com>
2017-03-03 15:44:36 +0200

    Improve actions progress bar

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def progress_tag(current:, min: 0, max: 100)
-    width_persentage = current.zero? ? 0 : current * 100 / max
+    width_persentage = current.zero? ? 0 : (current - min) * 100 / (max - min)
     content_tag(:div,
                 content_tag(:div, '',
                             class: 'progress-bar',
