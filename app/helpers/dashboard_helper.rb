06bc7073edeed6f3e01c74bdd13de056bfab1e35
Nikita Kholin <nik.kholin@gmail.com>
2017-03-15 01:19:55 +0200

    Fix progress bar when min < 0

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,6 @@
 module DashboardHelper
   def progress_tag(current:, min: 0, max: 100, text: '')
+    min, current, max = [min, current, max].map { |arg| arg + min * -1 } if min.negative?
     width_persentage = current.zero? ? 0 : (current - min) * 100 / (max - min)
     content_tag(:div,
                 content_tag(:div, text,
@@ -7,8 +8,7 @@ module DashboardHelper
                             'aria-valuemax' => max,
                             'aria-valuemin' => min,
                             'aria-valuenow' => current,
-                            'role' => 'progressbar',
-                            'style' => "width: #{width_persentage}%"),
+                            'role' => 'progressbar', 'style' => "width: #{width_persentage}%"),
                 class: 'progress')
   end
 
