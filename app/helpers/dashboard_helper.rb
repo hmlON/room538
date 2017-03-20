2466252e36a34338cc0dd0747ae0589829f73042
Nikita Kholin <nik.kholin@gmail.com>
2017-03-20 23:17:06 +0200

    Change progress bar to be red when < 0

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,13 +1,11 @@
 module DashboardHelper
+  # :reek:FeatureEnvy
   def progress_tag(current:, min: 0, max: 100, text: '')
-    min, current, max = [min, current, max].map { |arg| arg + (min * -1) - 1 } if min.negative?
     width_persentage = current.zero? ? 0 : (current - min) * 100 / (max - min)
     content_tag(:div,
                 content_tag(:div, text,
-                            class: 'progress-bar',
-                            'aria-valuemax' => max,
-                            'aria-valuemin' => min,
-                            'aria-valuenow' => current,
+                            class: current.negative? ? 'progress-bar bg-danger' : 'progress-bar',
+                            'aria-valuemax' => max, 'aria-valuemin' => min, 'aria-valuenow' => current,
                             'role' => 'progressbar', 'style' => "width: #{width_persentage}%"),
                 class: 'progress')
   end
