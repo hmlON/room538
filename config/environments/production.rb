7ec30db7a9a74cceb041d857675e80514b36173f
Nikita Kholin <nik.kholin@gmail.com>
2017-03-24 01:18:22 +0200

    Add email host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "room538_#{Rails.env}"
+  config.action_mailer.default_url_options = { host: 'room538.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
