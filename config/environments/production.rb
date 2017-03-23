201c0252f58677b1ce6dd9189d052ed47699a5ef
Nikita Kholin <nik.kholin@gmail.com>
2017-03-24 00:58:35 +0200

    Set up production email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,16 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "room538_#{Rails.env}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'mail.google.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
