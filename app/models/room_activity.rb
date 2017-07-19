ee5d4f64f3fcdb56e644a94023171d2d628ee491
Nikita Kholin <nik.kholin@gmail.com>
2017-07-20 00:12:33 +0300

    Create RoomActivity#next_on_user

--- app/models/room_activity.rb
+++ app/models/room_activity.rb
@@ -9,7 +9,16 @@ class RoomActivity < ApplicationRecord
     @user_activities ||= users.map { |user| UserActivity.new(user: user, room_activity: self) }
   end
 
+  def min_value
+    user_activities.map(&:value).min
+  end
+
   def max_value
     user_activities.map(&:value).max
+  end
+
+  def next_on_user
+    user_activities.find { |user_activity| user_activity.value == min_value }
+                   .user
   end
 end
--- spec/models/room_activity_spec.rb
+++ spec/models/room_activity_spec.rb
@@ -3,6 +3,17 @@ RSpec.describe RoomActivity, type: :model do
   let(:room_activity) { create :room_activity, room: room }
   subject { room_activity }
 
+  describe '#min_value' do
+    let(:user) { create :user, room: room }
+    let(:another_user) { create :user, room: room }
+    let!(:activities) { create_list(:activity, 1, room_activity: subject, user: user) }
+    let!(:another_activities) { create_list(:activity, 2, room_activity: subject, user: another_user) }
+
+    it 'returns minimum from all user activities values' do
+      expect(subject.min_value).to eq 1
+    end
+  end
+
   describe '#max_value' do
     let(:user) { create :user, room: room }
     let(:another_user) { create :user, room: room }
@@ -11,6 +22,17 @@ RSpec.describe RoomActivity, type: :model do
 
     it 'returns maximum from all user activities values' do
       expect(subject.max_value).to eq 2
+    end
+  end
+
+  describe '#next_on_user' do
+    let(:user) { create :user, room: room }
+    let(:another_user) { create :user, room: room }
+    let!(:activities) { create_list(:activity, 1, room_activity: subject, user: user) }
+    let!(:another_activities) { create_list(:activity, 2, room_activity: subject, user: another_user) }
+
+    it 'returns user who should do activity next' do
+      expect(subject.next_on_user).to eq user
     end
   end
 end
