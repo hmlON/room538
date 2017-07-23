374797474d239d0aede96150f1a756f294980e82
Nikita Kholin <nik.kholin@gmail.com>
2017-07-23 21:51:49 +0300

    Add extra setting trying to fix google auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
