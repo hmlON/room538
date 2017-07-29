12970b6bbb7c197c5495990af028243ccfb6cc57
Nikita Kholin <nik.kholin@gmail.com>
2017-07-29 22:31:05 +0300

    Create ApplicationService

---
+++ app/services/application_service.rb
@@ -0,0 +1,32 @@
+# to call: ApplicationService.(params)
+#
+class ApplicationService
+  def self.call(*args, **kwargs)
+    service = new(*args, **kwargs)
+    service.perform
+    service
+  end
+
+  # def initialize(*args, **kwargs)
+  #   kwargs.each do |method_name, method_value|
+  #     define_method(method_name) { method_value }
+  #   end
+  # end
+
+  def perform
+    raise NotImplementedError, 'This is an abstract method'
+  end
+
+  def errors
+    # http://api.rubyonrails.org/classes/ActiveModel/Errors.html
+    {}
+  end
+
+  def success?
+    errors.empty?
+  end
+
+  def failure?
+    !success?
+  end
+end
